e0230bce132123a5f4118b50203bfd0e1c974e9b
benjamin5250 <146529364+benjamin5250@users.noreply.github.com>
2023-10-14 20:49:13 +0630

    Update play.py

--- LegendMusic/utils/decorators/play.py
+++ LegendMusic/utils/decorators/play.py
@@ -23,7 +23,7 @@ def PlayWrapper(command):
         if PRIVATE_BOT_MODE == str(True):
             if not await is_served_private_chat(message.chat.id):
                 await message.reply_text(
-                    "**ᴩʀɪᴠᴀᴛᴇ ᴍᴜsɪᴄ ʙᴏᴛ**\n\nᴏɴʟʏ ғᴏʀ ᴛʜᴇ ᴄʜᴀᴛs ᴀᴜᴛʜᴏʀɪsᴇᴅ ʙʏ ᴛʜᴇ ᴏᴡɴᴇʀ. ʀᴇǫᴜᴇsᴛ ɪɴ ᴍʏ ᴏᴡɴᴇʀ's ᴩᴍ ᴛᴏ ᴀᴜᴛʜᴏʀɪsᴇ ʏᴏᴜʀ ᴄʜᴀᴛ ғᴏʀ ᴜsɪɴɢ ᴍᴇ."
+                    "**ᴩʀɪᴠᴀᴛᴇ ᴍᴜsɪᴄ ʙᴏᴛ**\n\n ᴏᴡɴᴇʀ ထံမှခွင့်ပြုချက်ရရှိသူများသာ အသုံးပြုနိုင်ပါသည်။ ခွင့်ပြုချက်တောင်းခံရန် ᴏᴡɴᴇʀ ထံသို့ဆက်သွယ်ပါ။"
                 )
                 return await app.leave_chat(message.chat.id)
         if await is_commanddelete_on(message.chat.id):
