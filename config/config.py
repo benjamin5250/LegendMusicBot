9f99d4b662d63dfeb68dee2a120e667da28ee629
benjamin5250 <146529364+benjamin5250@users.noreply.github.com>
2023-10-15 11:00:31 +0630

    Update config.py

--- config/config.py
+++ config/config.py
@@ -36,7 +36,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/benjamin5250/LegendMusicBot",
+    "https://github.com/TeamLegend77/LegendMusicBot",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
